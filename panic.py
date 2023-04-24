fa33ba255739abc7955d806aa211f530f5c78156
Peter Le <peter.le0349@gmail.com>
2023-04-24 08:51:50 -0500

    reorganize phrase

--- panic.py
+++ panic.py
@@ -4,8 +4,11 @@ print(phrase)
 print(plist)
 
 plist.pop(3)
+for x in range(4):
+    plist.pop()
 plist.pop(0)
-plist.insert(5,plist.pop(2))
+plist.insert(3,plist.pop(2))
+plist.append(plist.pop(4))
 
 
 
